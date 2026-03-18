8936eb7f238335712050c24138e4eb04a9646af0
SamuelVictorAnand <samuelvictor.a007@gmail.com>
2026-03-18 10:31:11 +0530

    Initial commit: UC1 setup

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,7 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("===== Book My Stay App =====");
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome to Hotel Booking System");
+    }
+}
